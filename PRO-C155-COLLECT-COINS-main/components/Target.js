b50269bb113a57b8026fd64ec7f8801abac7cb76
hanish2009 <75885981+hanish2009@users.noreply.github.com>
2023-09-17 17:33:30 +0530

    Add files via upload

---
+++ PRO-C155-COLLECT-COINS-main/components/Target.js
@@ -0,0 +1,42 @@
+// Registering component in Target.js
+
+AFRAME.registerComponent("target-ring", {
+  init: function () {
+    for (var i = 1; i <= 20; i++) {
+      //id
+      var id = `ring${i}`;
+
+      //position variables
+      var posX = Math.random() * 1200 + -1000;
+      var posY = Math.random() * 2 + -1;
+      var posZ = Math.random() * 1200 + -1000;
+
+      var position = { x: posX, y: posY, z: posZ };
+
+      //call the function
+      this.createRings(id, position);
+    }
+  },
+
+  createRings: function (id, position) {
+    var terrainEl = document.querySelector("#terrain");
+    var ringEl = document.createElement("a-entity");
+
+    ringEl.setAttribute("id", id);
+    ringEl.setAttribute("material", "color", "gold");
+    ringEl.setAttribute("position", position);
+    ringEl.setAttribute("geometry", { primitive: "cylinder", Height: 0.1, });
+
+    //set the static body attribute of physics system
+    ringEl.setAttribute("static-body", {
+      shape: "sphere",
+      sphereRadius: 2,
+    });
+
+    ringEl.setAttribute("game-play", {
+      elementId: `#${id}`,
+    });
+
+    terrainEl.appendChild(ringEl);
+  },
+});
